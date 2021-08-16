5f53923d9db15a0663d9cce51dee8d56f04814d2
tona44 <kuryu.waku@gmail.com>
2021-08-16 12:05:43 +0000

    app1-8 バリデーションの設定

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -12,21 +12,24 @@ class BlogsController < ApplicationController
   end
 
   def create
-    blog = Blog.new(blog_params)
-    blog.save
-    redirect_to blog_path(blog.id)
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to blog_path(@blog.id)
+    else
+      render :new
+    end
   end
 
   def edit
     @blog = Blog.find(params[:id])
   end
-  
+
   def update
     blog = Blog.find(params[:id])
     blog.update(blog_params)
     redirect_to blog_path(blog)
   end
-  
+
   def destroy
     blog = Blog.find(params[:id])
     blog.destroy
